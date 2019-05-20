77c535f37d75aaecdd3ad67fbf805285df859f90
Rémy Ramora <remy.ramora@coding-academy.fr>
2019-05-20 15:52:43 +0200

    ma page

--- portfolio.js
+++ portfolio.js
@@ -1,2 +1,9 @@
 
-window.onload();
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, true, 900, 250, 1000);
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, 0);
+    
+});
+
