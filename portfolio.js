e24ed8bede4e85d98b567f46e425952685861442
Rémy Ramora <remy.ramora@coding-academy.fr>
2019-05-22 17:28:40 +0200

    porfolio slider

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,10 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(elems, true, 900, 250, 1000);
+    var instances = M.Slider.init(elems, true, 700, 500, 4000);
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, 0);
-    
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, 275, 200, null, null, null, null);
 });
 
