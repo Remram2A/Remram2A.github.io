c4a8ce99d8af316fbda16972d3d98eb0f54d85e1
Rémy Ramora <remy.ramora@coding-academy.fr>
2019-05-29 20:58:15 +0200

    Formulaire

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,9 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(elems, true, 700, 500, 6000);
+    var instances = M.Slider.init(elems, true, 650, 500, 6000);
     var elems = document.querySelectorAll('.slider2');
-    var instances = M.Slider.init(elems, false, 700, 500, 6000);
+    var instances = M.Slider.init(elems, false, 650, 500, 6000);
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, 1);
     var elems = document.querySelectorAll('.materialboxed');
